b7ef5e39e2a7d9a07f950332d86cf034d8d52fee
Luca D'Angelo <info@lucadangelo.it>
2018-12-23 12:17:35 +0000

    Add project files.

---
+++ testToBeDeleted/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace testToBeDeleted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Derived Luca = new Derived();
+            Console.ReadLine();
+        }
+    }
+
+
+
+    public class Base
+    {
+        public Base()
+        {
+            Console.WriteLine("In base");
+        }
+    }
+    public class Derived : Base
+    {
+        public Derived()
+        {
+            Console.WriteLine("In derived");
+        }
+    }
+
+
+
+
+
+
+}
